f158808e48bdffc9371f52c585481f3c4231a2d2
lief <lief2liu@gmail.com>
2019-07-26 17:49:29 +0800

    es and redis enable

---
+++ src/main/java/top/dataframe/val/RedisInfoKey.java
@@ -0,0 +1,11 @@
+package top.dataframe.val;
+
+public interface RedisInfoKey {
+    String HOST = "redis.host";
+
+    String PORT = "redis.port";
+
+    String DB = "redis.db";
+
+    String PASSWORD = "redis.auth";
+}
---
+++ src/main/java/top/dataframe/writer/RedisWriter.groovy
@@ -0,0 +1,71 @@
+package top.dataframe.writer
+
+
+import groovy.transform.CompileStatic
+import redis.clients.jedis.Jedis
+import redis.clients.jedis.Pipeline
+import top.dataframe.DataFrame
+import top.dataframe.val.RedisInfoKey
+
+@CompileStatic
+class RedisWriter implements DataFrameWriter {
+    private Map<String, Object> connectInfo
+    String prefix
+    String key
+    String value
+
+    RedisWriter(Map<String, Object> connectInfo, String prefix) {
+        this.connectInfo = connectInfo
+        this.prefix = prefix
+        this.key = "key"
+        this.value = "value"
+    }
+
+    RedisWriter(Map<String, Object> connectInfo, String prefix, String key) {
+        this.connectInfo = connectInfo
+        this.prefix = prefix
+        this.key = key
+    }
+
+    RedisWriter(Map<String, Object> connectInfo, String prefix, String key, String value) {
+        this.connectInfo = connectInfo
+        this.prefix = prefix
+        this.key = key
+        this.value = value
+    }
+
+    @Override
+    int write(DataFrame df) {
+        Jedis jedis
+        try {
+            jedis = new Jedis(connectInfo.get(RedisInfoKey.HOST) as String, connectInfo.get(RedisInfoKey.PORT) as int)
+            def auth = connectInfo.get(RedisInfoKey.PASSWORD)
+            if (auth) {
+                jedis.auth(auth as String)
+            }
+            def db = connectInfo.get(RedisInfoKey.DB)
+            if (db) {
+                jedis.select(db as int)
+            }
+
+            Pipeline pip = jedis.pipelined()
+            df.rows.each { Map<String, Object> map ->
+                if (value) {
+                    pip.set(prefix + map.get(key), map.get(value) as String)
+                } else {
+                    // TODO
+                    return 0
+                }
+            }
+
+            pip.sync()
+
+            return df.size()
+        } finally {
+            jedis?.close()
+        }
+
+        0
+    }
+
+}
